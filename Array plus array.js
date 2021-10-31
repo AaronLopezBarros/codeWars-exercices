1aabb99d19eeb5340aa1c7486f47df65e4ad74e3
Aarón López Barros <aaronlopezbarros@MacBook-Pro-de-Ana.local>
2021-10-31 13:18:15 +0100

    Kata complete

--- Array plus array.js
+++ Array plus array.js
@@ -1,10 +1,10 @@
-INSTRUCTIONS
+// INSTRUCTIONS
 
 
-I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements.
-I'll appreciate for your help.
+// I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements.
+// I'll appreciate for your help.
 
-P.S. Each array includes only integer numbers. Output is a number too.
+// P.S. Each array includes only integer numbers. Output is a number too.
 
 
 
