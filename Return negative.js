9563b29a4a3247a22222babde705bd2fd8ac344e
Aaron López Barros <92530312+AaronLopezBarros@users.noreply.github.com>
2021-10-31 14:04:37 +0100

    Add files via upload

---
+++ Return negative.js
@@ -0,0 +1,12 @@
+//INSTRUCTIONS
+
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+
+function makeNegative(num) {
+    if(num === 0){
+      return 0
+    }else{
+      return num * -1
+    }
+  }
