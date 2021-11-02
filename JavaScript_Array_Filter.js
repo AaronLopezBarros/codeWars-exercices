7563d66b994a857029219c35cb1743b50c81473e
Aarón López <n_aaron5@hotmail.com>
2021-11-02 21:59:13 +0100

    javaScript_Array_Filter kata complete

---
+++ JavaScript_Array_Filter.js
@@ -0,0 +1,19 @@
+//INSTRUCTIONS
+
+// JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+// The solution would work like the following:
+
+// getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+
+
+
+
+function getEvenNumbers(numbersArray){
+    const newArray = numbersArray.filter((number)=>{
+      if(number % 2 === 0){
+        return true
+      }
+    })
+      return newArray
+    }
