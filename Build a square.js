c2ba7aeaaa7dd4bd5049f3909f3baa412292a13e
Aarón López <n_aaron5@hotmail.com>
2021-11-12 14:11:43 +0100

    Build a square kata complete

---
+++ Build a square.js
@@ -0,0 +1,22 @@
+//INSTRUCTIONS
+
+// I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+
+// Example
+// n = 3, so I expect a 3x3 square back just like below as a string:
+
+// +++
+// +++
+// +++
+
+function generateShape(integer){
+    let symbol = ''
+    let square = ''
+  for(i = 0; i < integer; i++){
+    symbol += '+'
+    }
+  for (i = 0; i < integer - 1; i++){
+    square += '\n' + symbol
+  }  
+    return symbol + square
+  }
